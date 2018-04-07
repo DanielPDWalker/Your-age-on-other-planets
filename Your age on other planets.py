d05617138d22be113a74d1ad8306aa8572d7a7c9
DanielPDWalker <danielpdwalker@gmail.com>
2018-04-07 15:11:07 +0100

    Full functionalilty (days and years), no error handling

--- Your age on other planets.py
+++ Your age on other planets.py
@@ -3,7 +3,6 @@ The terminal window will take in your age and a planet and,
 return your age on that planet.
 """
 
-import datetime as datetime
 import sys
 import time
 
@@ -16,9 +15,13 @@ class Planet:
         self.percent_day_earth_to_planet = days_percent
         self.percent_years_earth_to_planet = years_percent
 
-    def print_age_on_planet(self, age):
-        return "Your age on {} would be {} days old".format
-        (self.name, str(self.percent_day_earth_to_planet * (age * 365)))
+    def print_age_on_planet_in_days(self, age):
+        return "Your age on {} would be {} days old.".format(self.name,
+            str((age * 365) / self.percent_day_earth_to_planet))
+
+    def print_age_on_planet_in_years(self, age):
+        return "Your age on {} would be {} years old.".format(self.name,
+            str(age / self.percent_years_earth_to_planet))
 
 
 class KeyboardDisable():
@@ -39,9 +42,16 @@ class KeyboardDisable():
 
 
 global age
-Active = True
 disable_typing = KeyboardDisable()
+
+mercury = Planet("Mercury", 175.9, 0.241)
+venus = Planet("Venus", 116.8, 0.615)
 mars = Planet("Mars", 1.03, 1.88)
+jupiter = Planet("Jupiter", 0.414, 11.9)
+saturn = Planet("Saturn", 0.444, 29.4)
+uranus = Planet("Uranus", 0.718, 83.7)
+neptune = Planet("Neptune", 0.671, 163.7)
+pluto = Planet("Pluto", 0.671, 247.9)
 
 
 def type_out(text):
@@ -61,11 +71,26 @@ def check_input(saved_input):
         return False
 
 
-def check_planet_exists(saved_input):
-    if saved_input.lower() == "mars":
-        return mars.print_age_on_planet(age)
+def check_planet(saved_input):
+    if saved_input.lower() == "mercury":
+        return mercury.print_age_on_planet_in_days(age), mercury.print_age_on_planet_in_years(age)
+    elif saved_input.lower() == "venus":
+        return venus.print_age_on_planet_in_days(age), venus.print_age_on_planet_in_years(age)
+    elif saved_input.lower() == "mars":
+        return mars.print_age_on_planet_in_days(age), mars.print_age_on_planet_in_years(age)
+    elif saved_input.lower() == "jupiter":
+        return jupiter.print_age_on_planet_in_days(age), jupiter.print_age_on_planet_in_years(age)
+    elif saved_input.lower() == "saturn":
+        return saturn.print_age_on_planet_in_days(age), saturn.print_age_on_planet_in_years(age)
+    elif saved_input.lower() == "uranus":
+        return uranus.print_age_on_planet_in_days(age), uranus.print_age_on_planet_in_years(age)
+    elif saved_input.lower() == "neptune":
+        return neptune.print_age_on_planet_in_days(age), neptune.print_age_on_planet_in_years(age)
+    elif saved_input.lower() == "pluto":
+        return pluto.print_age_on_planet_in_days(age), pluto.print_age_on_planet_in_years(age)
     else:
-        raise ValueError("That planet doesnt exist.")
+        if restart_or_quit(saved_input):
+            return "{} is not a planet.".format(saved_input), ""
 
 
 def print_days_on_earth(age):
@@ -91,15 +116,24 @@ def menu():
 
 
 def main():
-    print("")
-    type_out("Please enter a planet you would like to see your age on: (Mars only)")
-    saved_input = input()
-    if not check_planet_exists(saved_input):
-        main()
-    else:
-        type_out(check_planet_exists(saved_input))
+    the_loop = True
+    type_out("If at any time you wish to restart of quit please enter: '!restart' or '!quit'")
+    while the_loop:
+        print("")
+        type_out("Please enter a planet you would like to see your age on:")
+        saved_input = input()
+        item1, item2 = check_planet(saved_input)
+        type_out(item1)
+        type_out(item2)
 
 
-while Active:
-    menu()
-    main()
+def restart_or_quit(saved_input):
+    if saved_input.lower() == "!restart":
+        menu()
+    if saved_input.lower() == "!quit":
+        exit()
+    return True
+
+
+menu()
+main()
