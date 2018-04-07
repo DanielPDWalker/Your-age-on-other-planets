a52cb7172ae19389e6d4c25cd25d84f11f8cedb0
DanielPDWalker <danielpdwalker@gmail.com>
2018-04-07 15:57:20 +0100

    Rounded output days and years to 3 decimal places. Doc strings and comments

--- Your age on other planets.py
+++ Your age on other planets.py
@@ -8,7 +8,7 @@ import time
 
 
 class Planet:
-    """Class for the planet objects"""
+    """Class for the planet objects."""
 
     def __init__(self, planet_name, days_percent, years_percent):
         self.name = planet_name
@@ -16,15 +16,22 @@ class Planet:
         self.percent_years_earth_to_planet = years_percent
 
     def print_age_on_planet_in_days(self, age):
+        """Returns the formatted string with the users age
+        on the selected planet in days.
+        """
         return "Your age on {} would be {} days old.".format(self.name,
-            str((age * 365) / self.percent_day_earth_to_planet))
+            str(round(float((age * 365) / self.percent_day_earth_to_planet), 3)))
 
     def print_age_on_planet_in_years(self, age):
+        """Returns the formatted string with the users age
+        on the selected planet in years.
+        """
         return "Your age on {} would be {} years old.".format(self.name,
-            str(age / self.percent_years_earth_to_planet))
+            str(round(float(age / self.percent_years_earth_to_planet), 3)))
 
 
 class KeyboardDisable():
+    """Makes an object with which you can disable keyboard input."""
 
     def __init__(self):
         self.on = False
@@ -41,9 +48,13 @@ class KeyboardDisable():
             msvcrt.getwch()
 
 
+# Users age.
 global age
+
+# This sets the object to disable the keyboard. Used in type_out().
 disable_typing = KeyboardDisable()
 
+# Initialise all the planets.
 mercury = Planet("Mercury", 175.9, 0.241)
 venus = Planet("Venus", 116.8, 0.615)
 mars = Planet("Mars", 1.03, 1.88)
@@ -51,10 +62,13 @@ jupiter = Planet("Jupiter", 0.414, 11.9)
 saturn = Planet("Saturn", 0.444, 29.4)
 uranus = Planet("Uranus", 0.718, 83.7)
 neptune = Planet("Neptune", 0.671, 163.7)
-pluto = Planet("Pluto", 0.671, 247.9)
+pluto = Planet("Pluto", 6.39, 247.9)
 
 
 def type_out(text):
+    """First disables keyboard input then prints out the passed
+    text's characters over time in the console/terminal window.
+    """
     disable_typing.start()
     text = text + "\n"
     for c in text:
@@ -65,6 +79,7 @@ def type_out(text):
 
 
 def check_input(saved_input):
+    """Checks for yes and no awnsers from the user."""
     if saved_input.lower() == "!yes":
         return True
     if saved_input.lower() == "!no":
@@ -72,6 +87,15 @@ def check_input(saved_input):
 
 
 def check_planet(saved_input):
+    """Checks the users input.lower() against the planet names.
+    If a planet is selected it will return the users age in days
+    and years on that planet.
+
+    Else this checks if the user wishes to restart or quit.
+
+    If it passes the restart or quit check, it will return that
+    the users input is not a planet.
+    """
     if saved_input.lower() == "mercury":
         return mercury.print_age_on_planet_in_days(age), mercury.print_age_on_planet_in_years(age)
     elif saved_input.lower() == "venus":
@@ -94,10 +118,19 @@ def check_planet(saved_input):
 
 
 def print_days_on_earth(age):
-        return "You are {} earth days old.".format(age * 365)
+    """Prints your age in earth days"""
+    return "You are {} earth days old.".format(age * 365)
 
 
 def menu():
+    """Runs the menu and set up, asking for users age.
+
+    Makes sure the age passed is an int, and handles any exceptions
+    by just restarting.
+
+    This also prints the users age in earth days before calling
+    the main() function.
+    """
     global age
     print("")
     print("##################################################")
@@ -119,6 +152,13 @@ def menu():
 
 
 def main():
+    """Main loop and functionality.
+
+    Will only type the how to restart or quit line once, then loops
+    through asking for planets to check user age on.
+
+    check_planet() ended up handling checking for restart or quit.
+    """
     the_loop = True
     type_out("If at any time you wish to restart of quit please enter: '!restart' or '!quit'")
     while the_loop:
@@ -131,6 +171,7 @@ def main():
 
 
 def restart_or_quit(saved_input):
+    """Check if the user has input the restart or quit command."""
     if saved_input.lower() == "!restart":
         menu()
     if saved_input.lower() == "!quit":
@@ -138,5 +179,6 @@ def restart_or_quit(saved_input):
     return True
 
 
+# Run the actual "program". Looping/flow is handled within the functions.
 menu()
 main()
